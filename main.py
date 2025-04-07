4abd194b7f2aca424381c001af1ae325bedfc2eb
Quyên <11520320@gm.uit.edu.vn>
2025-04-07 14:28:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,9 @@ def check_devices():
             
             if time_diff > timedelta(minutes=30):
                 ip = device_info.get('ip', 'Unknown IP')
-                outdated_devices.append(ip)
+                pub = device_info.get('pub', 'Unknown Pub')
+                offline_device = f"{ip} ({pub})"
+                outdated_devices.append(offline_device)
         
         # Nếu có device quá hạn, gửi thông báo qua Pushover
         if outdated_devices:
